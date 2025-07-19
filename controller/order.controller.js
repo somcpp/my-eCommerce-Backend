533c8e9f7d173fb2f6881c4ff4512090e68ceac9
somcpp <saubhagya.sharma3.14@gmail.com>
2025-07-19 12:31:40 +0530

    added admin orders

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,9 +1,22 @@
 import { Order } from "../models/order.model.js";
 
+export const fetchAllOrders = async (req,res) => {
+    let query = Order.find({deleted:{$ne:true}});
+    if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+    }
+    try {
+      const docs = await query.exec();
+      res.status(200).json(docs);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+}
+
 export const fetchOrdersByUser = async (req,res) => {
-    const {user} = req.query;
+    const {userId} = req.params;
     try{
-        const orders = await Order.find({user:user})
+        const orders = await Order.find({user:userId})
         res.status(200).json(orders);
     } catch (err){
         res.status(400).json(err)
@@ -22,8 +35,9 @@ export const createOrder = async (req,res) => {
 
 export const updateOrder = async (req,res) => {
     const {id} = req.params;
+    console.log(req.body)
     try{
-        const order = await order.findByIdAndUpdate(id,req.body, {
+        const order = await Order.findByIdAndUpdate(id,req.body, {
             new: true
         });
         res.status(200).json(order);
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createOrder, deleteOrder, fetchOrdersByUser, updateOrder } from '../controller/order.controller.js';
+import { createOrder, deleteOrder, fetchAllOrders, fetchOrdersByUser, updateOrder } from '../controller/order.controller.js';
 
 const router = express.Router();
 
 router.post('/',createOrder);
-router.get('/',fetchOrdersByUser);
+router.get('/user/:userId',fetchOrdersByUser);
+router.get('/',fetchAllOrders);
 router.delete('/:id',deleteOrder);
 router.patch('/:id',updateOrder);
+
 
 export default router;
